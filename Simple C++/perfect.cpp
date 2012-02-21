4d06da9023120571bff1ff4d68a67597b3c70492
dhbikoff <dhbikoff@gmail.com>
2012-02-21 18:55:48 -0500

    replaced \t with spaces

--- Simple C++/perfect.cpp
+++ Simple C++/perfect.cpp
@@ -11,12 +11,12 @@ const int MAX_NUM = 10000; // maximum number to test
 bool IsPerfect(int num);
 
 int main() {
-	for (int i = 0; i < MAX_NUM; i++) {
-		if (IsPerfect(i)) {
-			cout << i << endl;
-		}
-	}
-	return 0;
+    for (int i = 0; i < MAX_NUM; i++) {
+        if (IsPerfect(i)) {
+            cout << i << endl;
+        }
+    }
+    return 0;
 }
 
 /*
@@ -25,11 +25,11 @@ int main() {
 * itself.
 */
 bool IsPerfect(int num) {
-	int sum = 0;
-	for (int i = 0; i < num; i++) {
-		if (i > 0 && num % i == 0) {
-			sum += i;
-		}
-	}
-	return (num > 0 && sum == num);
+    int sum = 0;
+    for (int i = 0; i < num; i++) {
+        if (i > 0 && num % i == 0) {
+            sum += i;
+        }
+    }
+    return (num > 0 && sum == num);
 }
--- Simple C++/soundex.cpp
+++ Simple C++/soundex.cpp
@@ -20,15 +20,15 @@ char ParseChar(char c);
 
 int main ()
 {
-	// get name to encode
-	while(true) {
-		cout << "Enter surname (RETURN to quit): ";
-		string surname = GetLine();
-		if (surname == "") exit(0); // exits program
-		cout << "Soundex code for " << surname 
-			<< " is " << ParseName(surname) << endl;
-	}
-	return 0;
+    // get name to encode
+    while(true) {
+        cout << "Enter surname (RETURN to quit): ";
+        string surname = GetLine();
+        if (surname == "") exit(0); // exits program
+        cout << "Soundex code for " << surname 
+            << " is " << ParseName(surname) << endl;
+    }
+    return 0;
 }
 
 /*
@@ -36,32 +36,32 @@ int main ()
 * Soundex algorithm.
 */
 string ParseName(string name) {
-	string code;
-	name = ConvertToUpperCase(name);
-	for (int i = 0; i < name.length(); i++) {	
-		if (i == 0)
-			code += name[i]; // save first char
-		if (i > 0 && isalpha(name[i]) != 0)
-			code += ParseChar(name[i]);
-	}
-	
-	for (int j = 0; j < code.length(); j++) {
-		if (j > 0 && code[j] == code[j-1]) {
-			code.erase(j,1); // delete duplicates
-		} else if (code[j] == '0') {
-			code.erase(j,1); // delete zeros
-		}
-	}
-	
-	if (code.length() < CODE_LENGTH) {
-		int zeros = CODE_LENGTH - code.length();
-		for (int k = 0; k < zeros; k++) {
-			code += '0';  // padd zeros if code is too short
-		}
-	} else if (code.length() > CODE_LENGTH) {
-		code = code.substr(0,CODE_LENGTH); // truncate if code is too long
-	}
-	return code;
+    string code;
+    name = ConvertToUpperCase(name);
+    for (int i = 0; i < name.length(); i++) {    
+        if (i == 0)
+            code += name[i]; // save first char
+        if (i > 0 && isalpha(name[i]) != 0)
+            code += ParseChar(name[i]);
+    }
+    
+    for (int j = 0; j < code.length(); j++) {
+        if (j > 0 && code[j] == code[j-1]) {
+            code.erase(j,1); // delete duplicates
+        } else if (code[j] == '0') {
+            code.erase(j,1); // delete zeros
+        }
+    }
+    
+    if (code.length() < CODE_LENGTH) {
+        int zeros = CODE_LENGTH - code.length();
+        for (int k = 0; k < zeros; k++) {
+            code += '0';  // padd zeros if code is too short
+        }
+    } else if (code.length() > CODE_LENGTH) {
+        code = code.substr(0,CODE_LENGTH); // truncate if code is too long
+    }
+    return code;
 }
 
 /*
@@ -69,21 +69,21 @@ string ParseName(string name) {
 * alphabetic characters.
 */
 char ParseChar(char c) {
-	if (c == 'A' || c == 'E'|| c == 'I' || c == 'O' ||
-		c == 'U' || c == 'H' || c == 'W' || c == 'Y') {
-		return '0';	
-	} else if (c == 'B' || c == 'F' || c == 'P' || c == 'V') {
-		return '1';
-	} else if (c == 'C' || c == 'G' || c == 'J' || c == 'K' || 
-		c == 'Q' || c == 'S' || c == 'X' || c == 'Z') {
-		return '2';
-	} else if (c == 'D' || c == 'T') {
-		return '3';
-	} else if (c == 'M' || c == 'N') {
-		return '4';
-	} else if (c == 'L') {
-		return '5';
-	} else {
-		return '6';
-	}
+    if (c == 'A' || c == 'E'|| c == 'I' || c == 'O' ||
+        c == 'U' || c == 'H' || c == 'W' || c == 'Y') {
+        return '0';    
+    } else if (c == 'B' || c == 'F' || c == 'P' || c == 'V') {
+        return '1';
+    } else if (c == 'C' || c == 'G' || c == 'J' || c == 'K' || 
+        c == 'Q' || c == 'S' || c == 'X' || c == 'Z') {
+        return '2';
+    } else if (c == 'D' || c == 'T') {
+        return '3';
+    } else if (c == 'M' || c == 'N') {
+        return '4';
+    } else if (c == 'L') {
+        return '5';
+    } else {
+        return '6';
+    }
 }
