a89376cdcb94856c8069de06456de3bdfc4eea5c
dhbikoff <dhbikoff@gmail.com>
2012-03-07 16:00:40 -0500

    finished maze portion, but still need comments and cleanup

--- ADTs/mazerun.cpp
+++ ADTs/mazerun.cpp
@@ -6,19 +6,39 @@
 #include "grid.h"
 #include <iostream>
 #include "random.h"
+#include "queue.h"
+#include "stack.h"
+#include "simpio.h"
+#include "vector.h"
 
 const int TOTAL_ROWS = 17;
 const int TOTAL_COLUMNS = 30;
 
 void InitMaze(Maze &maze);
-pointT GetNeighborCell(pointT &pt);
+pointT GetNeighborCell(pointT &pt, Maze &maze);
+void SolveMaze(Maze &maze);
+void GetMorePaths(Maze &maze);
+bool ptAllowed(Vector<pointT> &usedPts, pointT &neighbor);
+void DrawPath(Stack<pointT> &path, Maze &maze);
+
+Stack<pointT> path;    
+Vector<pointT> usedPts;
+Queue<Stack<pointT> > pathCollection;        
+
 
 void MazeRun() {
     Randomize();
     InitGraphics();
     Maze maze(TOTAL_ROWS, TOTAL_COLUMNS, true);
     maze.draw();
-    InitMaze(maze);    
+    InitMaze(maze);
+    cout << "Press Enter to solve maze: ";
+    GetLine();
+    cout << endl;
+    SolveMaze(maze);
+    path.clear();
+    pathCollection.clear();
+    usedPts.clear();
 }
 
 void InitMaze(Maze &maze) {
@@ -32,7 +52,7 @@ void InitMaze(Maze &maze) {
     int includedCount = 0;
 
     while(includedCount < (TOTAL_ROWS * TOTAL_COLUMNS)-1) {
-        pointT neighbor = GetNeighborCell(pt);
+        pointT neighbor = GetNeighborCell(pt, maze);
         if (excluded(neighbor.row, neighbor.col)) {
             excluded(neighbor.row, neighbor.col) = false;
             maze.setWall(pt, neighbor, false);
@@ -41,10 +61,9 @@ void InitMaze(Maze &maze) {
         pt = neighbor;
         UpdateDisplay();
     }
-    cout << "Maze Complete." << endl << endl;
 }
 
-pointT GetNeighborCell(pointT &pt) {
+pointT GetNeighborCell(pointT &pt, Maze &maze) {
     pointT neighbor = pt;
     int rand = RandomInteger(1,4);
     switch(rand) {
@@ -60,9 +79,77 @@ pointT GetNeighborCell(pointT &pt) {
         default: 
             neighbor.row++;
     }
-    if (neighbor.row < 0 || neighbor.col < 0 || 
-        neighbor.row > TOTAL_ROWS-1 || neighbor.col > TOTAL_COLUMNS-1)
-        GetNeighborCell(pt);
+    if (!maze.pointInBounds(neighbor))
+        GetNeighborCell(pt, maze);
     else
         return neighbor;
+}
+
+void SolveMaze(Maze &maze) {
+    pointT startPt, finishPt;
+    startPt.row = 0;
+    startPt.col = 0;
+    finishPt.row = TOTAL_ROWS - 1;
+    finishPt.col = TOTAL_COLUMNS - 1;
+
+    path.push(startPt);
+    usedPts.add(startPt);
+    pathCollection.enqueue(path);
+
+    while(!pathCollection.isEmpty()) {
+        path = pathCollection.dequeue();
+        pointT pt = path.peek();
+        if (finishPt.row == pt.row && finishPt.col == pt.col) {
+            DrawPath(path, maze);
+            return;
+        } else {
+            GetMorePaths(maze);
+        }
+    }
+}
+
+void GetMorePaths(Maze &maze) {
+    pointT pt = path.peek();
+    for (int i = 0; i < 4; i++) {
+        pointT neighbor = pt;
+        switch(i) {
+            case 0: 
+                neighbor.col++;
+                break;
+            case 1: 
+                neighbor.col--;
+                break;
+            case 2: 
+                neighbor.row--;
+                break;
+            default: 
+                neighbor.row++;
+        }
+        if (ptAllowed(usedPts, neighbor) && 
+            maze.pointInBounds(neighbor) && !maze.isWall(pt, neighbor)) {
+            Stack<pointT> nextPath = path;
+            nextPath.push(neighbor);
+            pathCollection.enqueue(nextPath);
+            usedPts.add(neighbor);
+        }
+    }
+}
+
+bool ptAllowed(Vector<pointT> &usedPts, pointT &neighbor) {
+    for (int i = 0; i < usedPts.size(); i++) {
+        if (usedPts[i].row == neighbor.row && usedPts[i].col == neighbor.col)
+            return false;
+    }
+    return true;
+}
+
+void DrawPath(Stack<pointT> &path, Maze &maze) {
+    Vector<pointT> finalPath;
+    while(!path.isEmpty()) {
+        finalPath.add(path.pop());
+    }
+    for (int i = 0; i < finalPath.size(); i++) {
+        maze.drawMark(finalPath[i], "blue");
+        UpdateDisplay();
+    }
 }
