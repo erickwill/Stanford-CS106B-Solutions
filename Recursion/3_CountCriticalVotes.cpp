cbe940271de2ceb9ba8b1ca2effcf18719255c0a
dhbikoff <dhbikoff@gmail.com>
2012-03-20 16:07:16 -0400

    reformatted CountCriticalVotes

--- Recursion/3_CountCriticalVotes.cpp
+++ Recursion/3_CountCriticalVotes.cpp
@@ -5,30 +5,30 @@
 
 int CountCriticalVotes(Vector<int> & blocks, int blockIndex);
 int RecVote(Vector<int> &blocks, int index, 
-			int sum, int &count, int vote, int totalVotes);
+            int sum, int &count, int vote, int totalVotes);
 
 int main ()
 {
-	Vector<int> blocks;
-	int blockIndex;
-	while(true)
-	{
-		cout << "Enter vote blocks (enter 0 when finished): ";
-		int num = GetInteger();
-		if (num == 0) break;
-		blocks.add(num);
-	}
-	while(true)
-	{
-		cout << "Enter block index (starting at 0): ";
-		blockIndex = GetInteger();
-		if (blockIndex < blocks.size()) break;
-		else cout << "Invalid index" << endl;
-	}
-	int votes = CountCriticalVotes(blocks, blockIndex);
-	cout << "Block index " << blockIndex << " has " << votes
-		<< " critical votes." <<endl;
-	return 0;
+    Vector<int> blocks;
+    int blockIndex;
+    while(true)
+    {
+        cout << "Enter vote blocks (enter 0 when finished): ";
+        int num = GetInteger();
+        if (num == 0) break;
+        blocks.add(num);
+    }
+    while(true)
+    {
+        cout << "Enter block index (starting at 0): ";
+        blockIndex = GetInteger();
+        if (blockIndex < blocks.size()) break;
+        else cout << "Invalid index" << endl;
+    }
+    int votes = CountCriticalVotes(blocks, blockIndex);
+    cout << "Block index " << blockIndex << " has " << votes
+        << " critical votes." <<endl;
+    return 0;
 }
 
 int CountCriticalVotes(Vector<int> & blocks, int blockIndex)
@@ -42,26 +42,26 @@ int CountCriticalVotes(Vector<int> & blocks, int blockIndex)
   int totalVotes = 0;
   for (int i = 0; i < blocks.size(); i++) 
   {
-	totalVotes += blocks[i];
+    totalVotes += blocks[i];
   }
 
   return RecVote(blocks, index, sum, count, vote, totalVotes);
 }
 
 int RecVote(Vector<int> &blocks, int index, 
-			int sum, int &count, int vote, int totalVotes)
+            int sum, int &count, int vote, int totalVotes)
 {
-	if (index >= blocks.size())
-	{
-		int candidateA = sum;
-		int candidateB = totalVotes-sum;
-		if ((candidateB > candidateA && candidateA+vote > candidateB) ||
-			(candidateA > candidateB && candidateB+vote > candidateA))
-			count++;
-		return count;
-	}
-	// cast vote for A
-	RecVote(blocks, index+1, sum+blocks[index], count, vote, totalVotes);
-	// cast vote for B
-	RecVote(blocks, index+1, sum, count, vote, totalVotes);
+    if (index >= blocks.size())
+    {
+        int candidateA = sum;
+        int candidateB = totalVotes-sum;
+        if ((candidateB > candidateA && candidateA+vote > candidateB) ||
+            (candidateA > candidateB && candidateB+vote > candidateA))
+            count++;
+        return count;
+    }
+    // cast vote for A
+    RecVote(blocks, index+1, sum+blocks[index], count, vote, totalVotes);
+    // cast vote for B
+    RecVote(blocks, index+1, sum, count, vote, totalVotes);
 }
