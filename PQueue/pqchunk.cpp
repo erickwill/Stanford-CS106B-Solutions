038105ce68ec383ffa1ac8e74a8cae449fccf808
dhbikoff <dhbikoff@gmail.com>
2012-05-15 22:20:50 -0400

    initial commit

---
+++ PQueue/pqchunk.cpp
@@ -0,0 +1,232 @@
+#include "pqchunk.h"
+#include "genlib.h"
+#include <iostream>
+
+int const MaxElemsPerBlock = 4; // chunk size
+
+PQueue::PQueue()
+{
+    head = NULL;
+}
+
+PQueue::~PQueue()
+{
+    while (head != NULL) {
+        Cell *next = head->next;
+        delete head;
+        head = next;
+    }
+}
+
+void PQueue::print()
+{
+    for (Cell *cur = head; cur != NULL; cur = cur->next)
+    {
+        for (int i = 0; i < cur->BlocksInUse; i++)
+        {
+            cout << cur->values[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool PQueue::isEmpty()
+{
+    return (head == NULL);  
+}
+
+int PQueue::size()
+{
+    int count = 0;
+    for (Cell *cur = head; cur != NULL; cur = cur->next)
+        count += cur->BlocksInUse;
+    return count;
+}
+
+void PQueue::enqueue(int newValue)
+{
+    if (head == NULL)
+    {
+        initPQueue(newValue); // first value enqueued
+        return;
+    }
+    
+    Cell *cur, *prev, *newChunk = new Cell;
+    for (prev = NULL, cur = head; cur != NULL; prev=cur, cur = cur->next)
+    {
+        for (int i = 0; i < cur->BlocksInUse; i++)
+        {
+            if (newValue > cur->values[i])
+            {
+                // put in this cell if there is room
+                if (cur->BlocksInUse < MaxElemsPerBlock)
+                {
+                    shiftAdd(cur,newValue);
+                    return;
+                } 
+                // put in previous cell if there is room
+                else if (i == 0 && prev != NULL && prev->BlocksInUse < MaxElemsPerBlock)
+                {
+                    shiftAdd(prev,newValue);
+                    return;
+                }
+                // current and previous cells full, must split
+                else
+                {
+                    splitAdd(cur,newValue);
+                    return;
+                }
+            }
+        }
+    }
+
+    // insert in last cell
+    if (prev->BlocksInUse < MaxElemsPerBlock)
+    {
+        prev->values[prev->BlocksInUse] = newValue;
+        prev->BlocksInUse++;
+    }
+    // create new cell and add value
+    else
+    {
+        newChunk->values = new int[MaxElemsPerBlock];
+        newChunk->values[0] = newValue;
+        newChunk->BlocksInUse = 1;
+        newChunk->next = NULL;
+        prev->next = newChunk;
+    }
+}
+
+/* 
+* Searches cell to find appropriate insert point.
+* Shifts values to make room, then inserts value.
+*/
+void PQueue::shiftAdd(Cell *cell, int newValue)
+{
+    int index; // point of insertion
+    for (index = 0; index < MaxElemsPerBlock; index++)
+    {
+        if (newValue > cell->values[index])
+            break;
+    }
+    for (int i = MaxElemsPerBlock; i >= index; i--)
+    {
+        cell->values[i] = cell->values[i-1]; // shift values
+    }
+    cell->values[index] = newValue; // insert
+    cell->BlocksInUse++;
+}
+
+/*
+* Splits current Cell values between two Cells.
+* Searches for insertion point, then adds value.
+*/
+void PQueue::splitAdd(Cell *cur, int newValue)
+{
+    Cell *newChunk = new Cell;
+    newChunk->values = new int[MaxElemsPerBlock];
+    for (int i = MaxElemsPerBlock/2; i < MaxElemsPerBlock; i++)
+    {
+        newChunk->values[i-(MaxElemsPerBlock/2)] = cur->values[i];
+    }
+    newChunk->BlocksInUse = MaxElemsPerBlock/2;
+    cur->BlocksInUse = MaxElemsPerBlock/2;
+    
+    // insert into list
+    newChunk->next = cur->next;
+    cur->next = newChunk;
+
+    // check older Cell for insertion point
+    for (int j = 0; j < cur->BlocksInUse; j++)
+    {
+        if (newValue > cur->values[j])
+        {
+            shiftAdd(cur,newValue);
+            return;
+        }
+    }
+
+    // insert into new Cell
+    for (int k = 0; k < newChunk->BlocksInUse; k++)
+    {
+        if (newValue > newChunk->values[k])
+        {
+            shiftAdd(newChunk,newValue);
+            return;
+        }        
+    }
+}
+
+/*
+* Initializes first Cell and enqueues first value.
+*/
+void PQueue::initPQueue(int newValue)
+{
+    Cell *newChunk = new Cell;
+    newChunk->values = new int[MaxElemsPerBlock];
+    newChunk->values[0] = newValue;
+    newChunk->BlocksInUse = 1;
+    newChunk->next = NULL;
+    head = newChunk;
+}
+
+/*
+* Returns and removes the largest (first) value in the list.
+*/
+int PQueue::dequeueMax()
+{
+    if (isEmpty())
+        Error("Tried to dequeue max from an empty pqueue!");
+
+    int num = head->values[0]; // value to return
+
+    if (head->BlocksInUse > 1)
+    {
+        for (int i = 0; i < head->BlocksInUse-1; i++)
+        {
+            head->values[i] = head->values[i+1];
+        }
+    }
+
+    head->BlocksInUse--;
+
+    if (head->BlocksInUse == 0)
+    {
+         Cell *toBeDeleted = head;
+         head = head->next;     // splice head cell out
+        delete toBeDeleted;
+    }
+
+    return num;
+}
+
+int PQueue::bytesUsed()
+{
+    int total = sizeof(*this);
+     for (Cell *cur = head; cur != NULL; cur = cur->next)
+         total += sizeof(*cur);
+     return total;
+}
+
+string PQueue::implementationName()
+{
+    return "chunk list";
+}
+
+void PQueue::printDebuggingInfo()
+{
+   int count = 0;
+
+    cout << "------------------ START DEBUG INFO ------------------" << endl;
+    for (Cell *cur = head; cur != NULL; cur = cur->next) {
+        cout << "Cell #" << count << " (at address " << cur << ") values = ";
+        for (int i = 0; i < cur->BlocksInUse; i++)
+        {
+            cout << cur->values[i] << " ";
+        }
+       cout << " next = " << cur->next << endl;
+       count++;
+    }
+    cout << "------------------ END DEBUG INFO ------------------" << endl;
+}
---
+++ PQueue/pqchunk.h
@@ -0,0 +1,136 @@
+/*
+ * File: pqueue.h
+ * --------------
+ * Defines the interface for the priority queue class. 
+ * 
+ */
+ 
+#ifndef _pqchunk_h
+#define _pqchunk_h
+
+#include "genlib.h"
+#include "disallowcopy.h"
+
+/*
+ * Class: PQueue
+ * -------------
+ * This is the class for a priority queue.  This is not
+ * simple FIFO queue, it is a priority queue, where elements are
+ * retrieved in order of priority, not just by longevity in queue.
+ * The elements are integers and the integer is assumed to represent 
+ * the priority (larger integer is higher priority).
+ */
+class PQueue 
+{
+  public:
+    /*
+     * Constructor: PQueue
+     * Usage: PQueue pq;
+     *        PQueue *ppq = new PQueue;
+     * ---------------------------------
+     * Initializes a new pqueue to have no elements.  
+     */
+    PQueue();
+
+
+    /*
+     * Destructor: ~PQueue
+     * Usage: delete ppq;
+     * ------------------
+     * Deallocates all the storage associated with this pqueue.
+     */
+    ~PQueue();
+
+
+    /*
+     * Member function: isEmpty
+     * Usage: if (pq.isEmpty()) . . .
+     * -------------------------------
+     * Returns true if this pqueue contains no elements.
+     */
+    bool isEmpty();
+
+    /*
+     * Member function: size
+     * Usage: nElemes = pq.size();
+     * ---------------------------
+     * Returns number of elements contained in this pqueue.
+     */
+    int size();
+
+    /*
+     * Member function: enqueue
+     * Usage: pq.enqueue(val);
+     * -----------------------
+     * Adds the specified element to this pqueue. No effort is made to
+     * avoid duplicates. 
+     */
+    void enqueue(int newElem);
+
+    /*
+     * Member function: eequeueMax
+     * Usage: maxElem = pq.dequeueMax();
+     * ---------------------------------
+     * Removes the largest priority element from this pqueue and returns it.
+     * If this pqueue is empty, this function raises an error.
+     */
+    int dequeueMax();
+
+    /*
+     * Member function: bytesUsed
+     * Usage: numBytes = pq.bytesUsed();
+     * ----------------------------------
+     * This function would not usually be included as part of the class,
+     * but this is here as part of evaluating the tradeoffs betweem
+     * implementations. Given a pqueue, this function counts up
+     * and return the total amount of space used given its
+     * current contents.
+     */
+    int bytesUsed();
+        
+    /*
+     * Member function: implementationName
+     * Usage: cout << pq.implementationName();
+     * ---------------------------------------
+     * This operation would not usually be included as part of the class
+     * class, but is included to help with analyzing and reporting results.
+     * This member function returns a string that describes the PQueue
+     * implementation strategy ("sorted linked list", "vector", etc.).
+     */
+    string implementationName();
+
+    /*
+     * Member function: printDebuggingInfo
+     * Usage: pq.printDebuggingInfo();
+     * -------------------------------
+     * This operation would not usually be included as part of the class,
+     * but is included to give you a hook to put any helpful debugging
+     * print code (for example, something that prints the goopy details of
+     * the internal structure). You don't need to implement this routine and can
+     * ignore it entirely, but you may find it helpful to use as you are
+     * doing development.
+     */
+    void printDebuggingInfo();
+
+    /*
+     * Member function: print
+     * Usage: pq.();
+     * -------------------------------
+     * Prints all enqueued values.
+     */
+    void print();
+
+  private:
+    struct Cell {
+        int *values;
+        Cell *next;
+        int BlocksInUse;
+    };
+    Cell *head;
+    DISALLOW_COPYING(PQueue)
+    void initPQueue(int newValue);
+    void shiftAdd(Cell *cell, int newValue);
+    void splitAdd(Cell *cur, int newValue);
+};
+
+#endif
