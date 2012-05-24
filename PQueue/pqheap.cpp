eba0aa15ea6d0c8a5f59b6d328a3d929417cd3f5
dhbikoff <dhbikoff@gmail.com>
2012-05-24 15:41:35 -0400

    comments edit

--- PQueue/pqheap.cpp
+++ PQueue/pqheap.cpp
@@ -9,10 +9,9 @@
 
 PQueue::PQueue()
 {
-    // first value blank for easier sorting
-    entries.add(0);
+	// first value blank for easier sorting
+	entries.add(0);
 }
-
 
 PQueue::~PQueue()
 {
@@ -20,29 +19,29 @@ PQueue::~PQueue()
 
 void PQueue::print()
 {
-    for (int i = 0; i < entries.size(); i++)
-    {
-        cout << entries[i] << " ";
-    }
-    cout << endl;
+	for (int i = 0; i < entries.size(); i++)
+	{
+		cout << entries[i] << " ";
+	}
+	cout << endl;
 }
 
 bool PQueue::isEmpty()
 {
-    // adjust for empty first entry
-    if (entries.size() <= 1)
-        return true;
-    else
-        return false;
+	// adjust for empty first entry
+	if (entries.size() <= 1)
+		return true;
+	else
+		return false;
 }
 
 int PQueue::size()
 {
-    // adjust for empty first entry
-    if (entries.size() == 0)
-        return 0;
-    else
-        return entries.size()-1;
+	// adjust for empty first entry
+	if (entries.size() == 0)
+		return 0;
+	else
+		return entries.size()-1;
 }
 
 
@@ -51,14 +50,14 @@ int PQueue::size()
  */
 void PQueue::enqueue(int newValue)
 {
-    entries.add(newValue);
-    int index = entries.size()-1;
-    // move entry up heap
-    while (index > 1 && entries[index] > entries[index/2])
-    {        
-        swap(index,index/2);
-        index /= 2;
-    }
+	entries.add(newValue);
+	int index = entries.size()-1;
+	// move entry up heap
+	while (index > 1 && entries[index] > entries[index/2])
+	{		
+		swap(index,index/2);
+		index /= 2;
+	}
 }
 
 
@@ -66,62 +65,62 @@ void PQueue::enqueue(int newValue)
  * --------------------------------
  */
 int PQueue::dequeueMax()
-{    
-    if (isEmpty())
-        Error("Tried to dequeue max from an empty pqueue!");
+{	
+	if (isEmpty())
+		Error("Tried to dequeue max from an empty pqueue!");
 
-    int value = entries[1];
-    entries[1] = entries[entries.size()-1];
-    entries.removeAt(entries.size()-1);
-    int index = 1;
-    while (index*2 < entries.size())
-    {
-        // swap with right child
-        if ((index*2)+1 < entries.size() && 
-            entries[(index*2)+1] > entries[index*2] && 
-            entries[index] < entries[(index*2)+1])
-        {
-            swap(index,(index*2)+1);
-            index = (index*2)+1;
-        }
-        // swap with left child
-        else if (entries[index] < entries[index*2])
-        {
-            swap(index,index*2);
-            index = index*2;
-        }
-        // no more swapping necessary
-        else
-            break;
-    }
+	int value = entries[1];
+	entries[1] = entries[entries.size()-1];
+	entries.removeAt(entries.size()-1);
+	int index = 1;
+	while (index*2 < entries.size())
+	{
+		// swap with right child
+		if ((index*2)+1 < entries.size() && 
+			entries[(index*2)+1] > entries[index*2] && 
+			entries[index] < entries[(index*2)+1])
+		{
+			swap(index,(index*2)+1);
+			index = (index*2)+1;
+		}
+		// swap with left child
+		else if (entries[index] < entries[index*2])
+		{
+			swap(index,index*2);
+			index = index*2;
+		}
+		// no more swapping necessary
+		else
+			break;
+	}
 
-    return value;
+	return value;
 }
 
 void PQueue::swap(int indexA, int indexB)
 {
-    int tmp = entries[indexA];
-    entries[indexA] = entries[indexB];
-    entries[indexB] = tmp;
+	int tmp = entries[indexA];
+	entries[indexA] = entries[indexB];
+	entries[indexB] = tmp;
 }
-        
+		
 int PQueue::bytesUsed() 
 {
-    return sizeof(*this) + entries.bytesUsed();
+	return sizeof(*this) + entries.bytesUsed();
 }
 
 
 string PQueue::implementationName()
 {
-    return "heap";
+	return "heap";
 }
 
 void PQueue::printDebuggingInfo()
 {
-    cout << "------------------ START DEBUG INFO ------------------" << endl;
-    cout << "Pqueue contains " << size() << " entries" << endl;
-    for (int i = 1; i < entries.size(); i++) 
-        cout << entries[i] << " ";
-    cout << endl;
-    cout << "------------------ END DEBUG INFO ------------------" << endl;
+	cout << "------------------ START DEBUG INFO ------------------" << endl;
+	cout << "Pqueue contains " << size() << " entries" << endl;
+	for (int i = 1; i < entries.size(); i++) 
+		cout << entries[i] << " ";
+	cout << endl;
+	cout << "------------------ END DEBUG INFO ------------------" << endl;
 }
--- PQueue/pqheap.h
+++ PQueue/pqheap.h
@@ -1,11 +1,9 @@
 /*
- * File: pqueue.h
+ * File: pqheap.h
  * --------------
  * Defines the interface for the priority queue class. 
  * 
- * Julie Zelenski, CS106, Fall 2007
  */
- 
  
 #ifndef _pqheap_h
 #define _pqheap_h
@@ -26,112 +24,112 @@ class PQueue
 {
   public:
 
-    /*
-     * Constructor: PQueue
-     * Usage: PQueue pq;
-     *        PQueue *ppq = new PQueue;
-     * ---------------------------------
-     * Initializes a new pqueue to have no elements.  
-     */
-    PQueue();
+	/*
+	 * Constructor: PQueue
+	 * Usage: PQueue pq;
+	 *        PQueue *ppq = new PQueue;
+	 * ---------------------------------
+	 * Initializes a new pqueue to have no elements.  
+	 */
+	PQueue();
 
 
-    /*
-     * Destructor: ~PQueue
-     * Usage: delete ppq;
-     * ------------------
-     * Deallocates all the storage associated with this pqueue.
-     */
-    ~PQueue();
+	/*
+	 * Destructor: ~PQueue
+	 * Usage: delete ppq;
+	 * ------------------
+	 * Deallocates all the storage associated with this pqueue.
+	 */
+	~PQueue();
 
 
-    /*
-     * Member function: isEmpty
-     * Usage: if (pq.isEmpty()) . . .
-     * -------------------------------
-     * Returns true if this pqueue contains no elements.
-     */
-    bool isEmpty();
+	/*
+	 * Member function: isEmpty
+	 * Usage: if (pq.isEmpty()) . . .
+	 * -------------------------------
+	 * Returns true if this pqueue contains no elements.
+	 */
+	bool isEmpty();
 
 
-    /*
-     * Member function: size
-     * Usage: nElemes = pq.size();
-     * ---------------------------
-     * Returns number of elements contained in this pqueue.
-     */
-    int size();
+	/*
+	 * Member function: size
+	 * Usage: nElemes = pq.size();
+	 * ---------------------------
+	 * Returns number of elements contained in this pqueue.
+	 */
+	int size();
 
 
-    /*
-     * Member function: enqueue
-     * Usage: pq.enqueue(val);
-     * -----------------------
-     * Adds the specified element to this pqueue. No effort is made to
-     * avoid duplicates. 
-     */
-    void enqueue(int newElem);
+	/*
+	 * Member function: enqueue
+	 * Usage: pq.enqueue(val);
+	 * -----------------------
+	 * Adds the specified element to this pqueue. No effort is made to
+	 * avoid duplicates. 
+	 */
+	void enqueue(int newElem);
 
 
-    /*
-     * Member function: eequeueMax
-     * Usage: maxElem = pq.dequeueMax();
-     * ---------------------------------
-     * Removes the largest priority element from this pqueue and returns it.
-     * If this pqueue is empty, this function raises an error.
-     */
-    int dequeueMax();
+	/*
+	 * Member function: eequeueMax
+	 * Usage: maxElem = pq.dequeueMax();
+	 * ---------------------------------
+	 * Removes the largest priority element from this pqueue and returns it.
+	 * If this pqueue is empty, this function raises an error.
+	 */
+	int dequeueMax();
 
 
-    /*
-     * Member function: bytesUsed
-     * Usage: numBytes = pq.bytesUsed();
-     * ----------------------------------
-     * This function would not usually be included as part of the class,
-     * but this is here as part of evaluating the tradeoffs betweem
-     * implementations. Given a pqueue, this function counts up
-     * and return the total amount of space used given its
-     * current contents.
-     */
-    int bytesUsed();
-    
-    
-    /*
-     * Member function: implementationName
-     * Usage: cout << pq.implementationName();
-     * ---------------------------------------
-     * This operation would not usually be included as part of the class
-     * class, but is included to help with analyzing and reporting results.
-     * This member function returns a string that describes the PQueue
-     * implementation strategy ("sorted linked list", "vector", etc.).
-     */
-    string implementationName();
+	/*
+	 * Member function: bytesUsed
+	 * Usage: numBytes = pq.bytesUsed();
+	 * ----------------------------------
+	 * This function would not usually be included as part of the class,
+	 * but this is here as part of evaluating the tradeoffs betweem
+	 * implementations. Given a pqueue, this function counts up
+	 * and return the total amount of space used given its
+	 * current contents.
+	 */
+	int bytesUsed();
+	
+	
+	/*
+	 * Member function: implementationName
+	 * Usage: cout << pq.implementationName();
+	 * ---------------------------------------
+	 * This operation would not usually be included as part of the class
+	 * class, but is included to help with analyzing and reporting results.
+	 * This member function returns a string that describes the PQueue
+	 * implementation strategy ("sorted linked list", "vector", etc.).
+	 */
+	string implementationName();
 
 
-    /*
-     * Member function: printDebuggingInfo
-     * Usage: pq.printDebuggingInfo();
-     * -------------------------------
-     * This operation would not usually be included as part of the class,
-     * but is included to give you a hook to put any helpful debugging
-     * print code (for example, something that prints the goopy details of
-     * the internal structure). You don't need to implement this routine and can
-     * ignore it entirely, but you may find it helpful to use as you are
-     * doing development.
-     */
-    void printDebuggingInfo();
-    
-    /*
-     * Member function: print
-     * Usage: pq.print()
-     * -------------------------------
-     * Prints all values in the vector, including the empty first value.
-     */
-    void print();
+	/*
+	 * Member function: printDebuggingInfo
+	 * Usage: pq.printDebuggingInfo();
+	 * -------------------------------
+	 * This operation would not usually be included as part of the class,
+	 * but is included to give you a hook to put any helpful debugging
+	 * print code (for example, something that prints the goopy details of
+	 * the internal structure). You don't need to implement this routine and can
+	 * ignore it entirely, but you may find it helpful to use as you are
+	 * doing development.
+	 */
+	void printDebuggingInfo();
+	
+	/*
+	 * Member function: print
+	 * Usage: pq.print()
+	 * -------------------------------
+	 * Prints all values in the vector, including the empty first value.
+	 */
+	void print();
 
   private:
-    Vector<int> entries;
-    void swap(int indexA, int indexB);
+	Vector<int> entries;
+	void swap(int indexA, int indexB);
 };
 
 #endif
